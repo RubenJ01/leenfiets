e38a63c1a323a83dfb78a6aa08fd1d91887618f0
Anton Bonder <anton.bonder@hotmail.com>
2019-10-04 16:00:40 +0200

    Aanpassing gedaan in fiets_toevoegen.
    Werkt nog niet.

--- fiets_toevoegen.php
+++ fiets_toevoegen.php
@@ -17,40 +17,63 @@ require 'utils/database_connection.php';
 <body>
 <div><?php include 'menu.php'; ?></div>
 <form method="post" id="fietsentoevoegen" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-    Merk: <input type="text" name="merk" placeholder="Merk"><br>
+    Merk
+    <select name="merk_naam">
+        <?php
+        $sql = "SELECT merk_naam, id FROM merk_fiets order by merk_naam asc";
+        $result = $mysqli->query($sql);
+        while($row = $result->fetch_assoc())
+        {
+            ?>
+            <option value = <?php echo($row['id'])?> >
+                <?php echo($row['merk_naam']) ?>
+            </option>
+            <?php
+        }
+        ?>
+        <option value="anders">Anders</option>
+    </select><br>
     Model: <input type="text" name="model" placeholder="Model"><br>
     Plaats: <input type="text" name="plaats" placeholder="Plaats"><br>
+    Adres: <input type="text" name="adres" placeholder="Adres"><br>
     Kleur fiets:
-    <select name="merk">
-        <option value="geel">Geel</option>
-        <option value="oranje">Oranje</option>
-        <option value="zwart">Zwart</option>
-        <option value="blauw">Blauw</option>
-        <option value="grijs">Grijs</option>
-        <option value="wit">Wit</option>
+    <select name="kleur">
+        <option value="Geel">Geel</option>
+        <option value="Oranje">Oranje</option>
+        <option value="Zwart">Zwart</option>
+        <option value="Blauw">Blauw</option>
+        <option value="Grijs">Grijs</option>
+        <option value="Wit">Wit</option>
     </select><br>
     Man of vrouw<input type="radio" name="soort_fiets" value="Mannen fiets">Mannen fiets
     <input type="radio" name="soort_fiets" value="Vrouwen fiets">Vrouwen fiets<br>
     Versnellingen: <input type="number" name="merk" placeholder="Aantal versnellingen"><br>
     Soort fiets:
-    <select name="merk">
+    <select name="soort_fiets">
         <?php
-        $sql = "SELECT soort_fiets FROM soort_fiets order by soort_fiets asc";
+        $sql = "SELECT soort_fiets, id FROM soort_fiets order by soort_fiets asc";
         $result = $mysqli->query($sql);
         while($row = $result->fetch_assoc())
         {
         ?>
-        <option value = "<?php echo "`Aantal cilinders` = " .($row['soort_fiets'])?>" >
+        <option value = <?php echo($row['id'])?> >
             <?php echo($row['soort_fiets']) ?>
         </option>
             <?php
         }
         ?>
+        <option value="anders">Anders</option>
     </select><br>
     Borg: <input type="number" name="borg" placeholder="Borg"><br>
     Huurprijs per dag:<input type="number" name="huur-prijs" placeholder="Huurprijs"><br>
-    <input type="file" name="foto" value="foto"><br>
-    <input type="submit" value="Toevoegen">
+    <input type="file" name="foto" value="foto"><br><br>
+    <input type="submit" name="toevoegen" value="Toevoegen">
 </form>
+<?php
+if(isset($_POST['toevoegen'])){
+    $sql = "INSERT INTO fietsen(borg, prijs, gebruiker_id, plaats, id_soort_fiets, id_merk_fiets, adres, foto_id) VALUES (".$_POST['merk_naam'].",2,3,'".$_POST['plaats']."',".$_POST['soort_fiets'].",3,'".$_POST['adres']."',3);";
+    $insert_query = $mysqli->query($sql);
+}
+?>
 </body>
 </html>
