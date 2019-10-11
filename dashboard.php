aef73a54b2a7478c020c4fd7ce335699f103174b
RubenJ01 <45236226+RubenJ01@users.noreply.github.com>
2019-10-11 19:10:15 +0200

    je kan op het dashboard nu merken toevoegen en verwijderen

--- dashboard.php
+++ dashboard.php
@@ -76,6 +76,16 @@ if (isset($_SESSION['rol'])) {
             }
             ?>
         </table>
+        <form method="post" action="" id="merk_aanpassen">
+            <h3>Merk Toevoegen</h3>
+            <label for="nieuw_merk">Merk:</label>
+            <input type="text" name="merk_naam" id="merk_naam" placeholder="Merk naam"> <br />
+            <label for="merk_toevoegen">Toevoegen</label>
+            <input type="radio" name="merk_aanpassen" id="merk_toevoegen" value="toevoegen"> <br />
+            <label for="merk_verwijderen">Verwijderen</label>
+            <input type="radio" name="merk_aanpassen" id="merk_verwijderen" value="verwijderen"> <br />
+            <input type="submit" name="merk_aanpassing_versturen" value="Uitvoeren" class="registreerknop">
+        </form>
     </div>
     <script src="js/dashboard.js"></script>
 </body>
@@ -84,5 +94,33 @@ if (isset($_SESSION['rol'])) {
     }
 } else {
     RedirectToPage('index.php');
+}
+
+// controleren of we de goeie form behandelen
+if (isset($_POST['merk_aanpassing_versturen'])) {
+    $merk_naam = $_POST['merk_naam'];
+    $aanpassing = $_POST['merk_aanpassen'];
+    if ($aanpassing == 'toevoegen'){
+        $check_bestaan = "select merk_naam from merk_fiets where merk_naam = '$merk_naam'";
+        $result_check = $mysqli->query($check_bestaan);
+        if ($result_check->num_rows == 0){
+            $voeg_toe = "insert into merk_fiets(merk_naam)
+                         values('$merk_naam')";
+            $result = $mysqli->query($voeg_toe);
+            echo $merk_naam . " toegevoegd aan de database.";
+        } else {
+            echo "Dat merk bestaat al.";
+        }
+    } elseif ($aanpassing == 'verwijderen'){
+        $check_bestaan = "select merk_naam from merk_fiets where merk_naam = '$merk_naam'";
+        $result = $mysqli->query($check_bestaan);
+        if ($result->num_rows == 1){
+            $verwijder = "delete from merk_fiets where merk_naam = '$merk_naam' limit 1";
+            $result = $mysqli->query($verwijder);
+            echo $merk_naam . " verwijdert uit de database.";
+        } else {
+            echo "Dat merk bestaat al.";
+        }
+    }
 }
 ?>
--- index.php
+++ index.php
@@ -10,7 +10,6 @@ if (!isset($_SESSION)) {
     session_start();
 }
 require 'utils/database_connection.php';
-require 'fietsen/qrcode.php';
 ?>
 <!DOCTYPE html>
 <html lang="nl">
@@ -21,69 +20,9 @@ require 'fietsen/qrcode.php';
 <body>
     <div><?php include 'menu.php'; ?></div>
     <?php
-    //GenerateQR(1);
-    // Check of de gebruiker de qr code heeft gescant
-    if (isset($_GET['qr']) && isset($_GET['fietsId']) && isset($_GET['token'])) {
-      echo "test";
-      $fietsId = $_GET['fietsId'];
-      $token = $_GET['token'];
-      // Voor query uit om de status en de token van de juiste fiets op te halen
-      $query = "SELECT status, token
-                FROM fietsen
-                WHERE id=?";
-      $stmt = $mysqli->prepare($query);
-      if (!$stmt) {
-        trigger_error($mysqli->error, E_USER_ERROR);
-      }
-      else {
-        $stmt->bind_param('i', $fietsId);
-        if (!$stmt->execute()) {
-          trigger_error($stmt->error, E_USER_ERROR);
-        }
-        $status = null;
-        $tokenDB = null;
-        $stmt->bind_result($status, $tokenDB);
-        $stmt->fetch();
-        $stmt->close();
-        echo "{$tokenDB} en $token";
-        // Kijk of de token matcht met de token in de database
-        if ($tokenDB !== null && $token === $tokenDB) {
-          // Delete de qr code naar dat die succesvol is gescant
-          DeleteQR($fietsId);
-          // Verander de status in de database
-          if ($status == "beschikbaar") {
-            $status = "uitgeleend";
-          }
-          else {
-            $status = "beschikbaar";
-          }
-          $query = "UPDATE fietsen
-                    SET status = ?, token = NULL
-                    WHERE id = ?";
-          $stmt2 = $mysqli->prepare($query);
-          if (!$stmt2) {
-            trigger_error($mysqli->error, E_USER_ERROR);
-          }
-          else {
-            $copyFietsId = $fietsId;
-            $stmt2->bind_param('si', $status, $copyFietsId);
-            if (!$stmt2->execute()) {
-              trigger_error($stmt2->error, E_USER_ERROR);
-            }
-            // Geef een bericht aan de gebruiker over de status van de fiets
-            if ($status == "beschikbaar") { echo "Je hebt de fiets met succes teruggebracht"; } else { echo "Je hebt de fiets met succes opgehaalt!"; }
-            $stmt2->close();
-          }
-        }
-      }
-    }
-
-
-
-
-    $sql = "SELECT fietsen.borg, fietsen.prijs, fietsen.versnellingen, fietsen.id, fietsen.plaats, fietsen.kleur_fiets, fietsen.model, fietsen.geslacht_fiets, fietsen.adres, fietsen.foto, soort_fiets.soort_fiets, merk_fiets.merk_naam
-        from fietsen, merk_fiets, soort_fiets
-        WHERE fietsen.id_soort_fiets = soort_fiets.id
+    $sql = "SELECT fietsen.borg, fietsen.prijs, fietsen.versnellingen, fietsen.id, fietsen.plaats, fietsen.kleur_fiets, fietsen.model, fietsen.geslacht_fiets, fietsen.adres, fietsen.foto, soort_fiets.soort_fiets, merk_fiets.merk_naam 
+        from fietsen, merk_fiets, soort_fiets   
+        WHERE fietsen.id_soort_fiets = soort_fiets.id 
         AND fietsen.id_merk_fiets = merk_fiets.id ";
     $result = $mysqli->query($sql);
     if ($result->num_rows > 0) {
@@ -94,7 +33,7 @@ require 'fietsen/qrcode.php';
                 $afbeelding = "fiets_afbeeldingen/default.png";
 
             }
-            echo   "<div class= 'fiets_blok'>
+            echo   "<div class='fiets_blok'>
                     <h1>$row[merk_naam] $row[model]</h1>
                     <h4>€$row[prijs] per dag</h4>
                     <ul>
