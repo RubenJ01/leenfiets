632f6058fde0c85dc5471cd22f49af796aa5d3b5
Arjan14 <Arjan123QQ@gmail.com>
2019-10-11 11:43:42 +0200

    Je kan een eigen bio toevoegen en updaten die vervolgens word weergegeven op je profiel

--- profiel.php
+++ profiel.php
@@ -4,24 +4,34 @@ require 'utils/database_connection.php';
 if (!isset($_SESSION)) {
     session_start();
 }
-?>
 
-<?php
 if (isset($_GET['gebruikers_id'])) {
 
 $GLOBALS['id'] = $_GET['gebruikers_id'];
-$sql_userinfo = "select naam, email from gebruiker where id = '$id'";
+$sql_userinfo = "select naam, email, bio from gebruiker where id = '$id'";
 $get_userinfo = $mysqli->query($sql_userinfo);
 
 while ($row = $get_userinfo->fetch_assoc()) {
     $naam = $row['naam'];
     $email = $row['email'];
+    $user_bio = $row['bio'];
 }
-?>
 
-<?php } else {
+} else {
     header('location: index.php');
-} ?>
+}
+
+if (isset($_POST['submit'])) {
+    $bio = $_POST['bio'];
+    $sql = "update gebruiker
+            set bio = '$bio'
+            where id = '$id'";
+    $bio_update = $mysqli->query($sql);
+
+}
+
+
+?>
 
 <!DOCTYPE html>
 <html lang="nl">
@@ -35,19 +45,27 @@ while ($row = $get_userinfo->fetch_assoc()) {
     <table>
         <tr><td>Profielfoto:</td><td><input type="file" name="foto" value="foto" id="foto"></td></tr>
         <tr><td><input type="submit" name="toevoegen" value="Toevoegen"></td></tr>
-    </table>
+    </table><br/>
 
-    <p>Gebruikersnaam: <?php echo $naam?></p>
-    <p>Email address: <?php echo $email?></p>
-    <p>Gebruiker ID: <?php echo $id?></p>
-    <p>Bio: </p><br/><br/>
+    <p>Gebruikersnaam: <?php echo $naam;?></p>
+    <p>Email address: <?php echo $email;?></p>
+    <p>Gebruiker ID: <?php echo $id;?></p>
+    <p>Bio: <?php echo $user_bio;?></p><br/><br/>
 
+<?php
+if (isset($_SESSION['id'])) {
+    if ($_SESSION['id'] == $_GET['gebruikers_id']) {
+        echo " 
     <p>Bio updaten:</p>
-    <form action="" method="post">
-        <textarea name="bio" cols="40" rows="5"></textarea><br/>
-        <input type="submit" name="submit" value="Update en verzenden">
+    <form action='' method='post'>
+        <textarea name='bio' cols='40' rows='5'></textarea><br/>
+        <input type='submit' name='submit' value='Update en verzenden'>
     </form>
+    ";
+    }
+}
 
-
+?>
 </body>
 </html>
+
