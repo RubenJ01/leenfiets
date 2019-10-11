507613727bf12eda91cd12f89b05d1a96493260d
Arjan14 <Arjan123QQ@gmail.com>
2019-10-11 10:18:18 +0200

    <title></title> title = naam

--- profiel.php
+++ profiel.php
@@ -5,37 +5,42 @@ if (!isset($_SESSION)) {
     session_start();
 }
 ?>
+
+<?php
+if (isset($_GET['gebruikers_id'])) {
+
+$GLOBALS['id'] = $_GET['gebruikers_id'];
+$sql_userinfo = "select naam, email from gebruiker where id = '$id'";
+$get_userinfo = $mysqli->query($sql_userinfo);
+
+while ($row = $get_userinfo->fetch_assoc()) {
+    $naam = $row['naam'];
+    $email = $row['email'];
+}
+?>
+
+<?php } else {
+    header('location: index.php');
+} ?>
+
 <!DOCTYPE html>
 <html lang="nl">
 <head>
-    <title><?php $GLOBALS['id'] = $_GET['gebruikers_id']; echo $id;?></title>
+    <title><?php $GLOBALS['id'] = $_GET['gebruikers_id']; echo $naam;?></title>
     <meta charset="UTF-8">
 </head>
 <body>
 <div><?php include 'menu.php'; ?></div>
-<?php
-if (isset($_GET['gebruikers_id'])) {
 
-    $GLOBALS['id'] = $_GET['gebruikers_id'];
-    $sql_userinfo = "select naam, email from gebruiker where id = '$id'";
-    $get_userinfo = $mysqli->query($sql_userinfo);
-
-    while ($row = $get_userinfo->fetch_assoc()) {
-        $naam = $row['naam'];
-        $email = $row['email'];
-    }
-?>
     <p>Gebruikersnaam: <?php echo $naam?></p>
     <p>Email address: <?php echo $email?></p>
     <p>Gebruiker ID: <?php echo $id?></p>
 
     <form action="" method="post">
-        <textarea name="Text1" cols="40" rows="5"></textarea>
+        <textarea name="bio" cols="40" rows="5"></textarea><br/>
+        <input type="submit" name="submit" value="update">
     </form>
 
-<?php } else {
-    header('location: index.php');
-    //jjij
-} ?>
+
 </body>
 </html>
