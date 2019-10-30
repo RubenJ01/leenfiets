ad0e931e7a8d84796651914f402d0646a6ef0e10
Arjan14 <Arjan123QQ@gmail.com>
2019-10-30 16:17:59 +0100

    Reviews komen in DB

--- review.php
+++ review.php
@@ -2,7 +2,11 @@
 <!DOCTYPE html>
 <?php
 include "menu.php";
-include "utils/core_functions.php";
+require 'utils/database_connection.php';
+require 'utils/core_functions.php';
+if (!isset($_SESSION)) {
+    session_start();
+}
 ?>
 <html lang="en" dir="ltr">
   <head>
@@ -13,21 +17,34 @@ include "utils/core_functions.php";
 
  <?php
 
- if (isset($_POST['submit'])) {
-     $sql = "INSERT INTO comments VALUES('','','')";
-     $review_update = $mysqli->query($sql);
-     header("Refresh:0");
- }
- //werkt nog niet
+
+
  ?>
 
+ <div class='profiel_fiets_text'>
+     <h2>Op deze pagina kunt u een review achterlaten over de wesite.<br> Wat vind u van de website?<br></h2>
+ </div>
 
-  <form action="" method="post">
-      <input type="text" name="name" value="Uw naam"><br>
-      <textarea name="comment" cols="50" rows="5">Laat een review achter</textarea><br>
-      <input type="submit" value="Laat review achter!">
+
+
+  <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+      <input type="text" name="name" value="<?php echo (isset($_POST['name']) ? $_POST['name'] : ''); ?>" required placeholder="Uw naam"><br>
+      <textarea name="comment" cols="50" rows="5" value="<?php echo (isset($_POST['comment']) ? $_POST['comment'] : ''); ?>" required placeholder="Review"></textarea><br>
+      <input type="submit" name="submit" value="Laat review achter!">
   </form>
 
+<?php
+
+if(isset($_POST['submit'])) {
+    $name = $_POST['name'];
+    $comment = $_POST['comment'];
+}
+
+$sql = "INSERT INTO `comments`(`name`, `comment`) VALUES ('$name', '$comment')";
+$result = $mysqli->query($sql);
+
+
+?>
 
 
   </body>
