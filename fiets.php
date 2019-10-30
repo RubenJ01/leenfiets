47c7be47f64e9256f39ee309b059e68d1b26994e
Arjan14 <Arjan123QQ@gmail.com>
2019-10-30 14:48:26 +0100

    Contactgegevens pagina aangemaakt, reviews pagina aangemaakt WIP

--- fiets.php
+++ fiets.php
@@ -135,11 +135,9 @@ if(isset($_GET['succesvol_toegevoegd'])){
         <tr><td>Adres</td><td> <?php echo $adres?></td></tr>
     </table>
     <div class="mapouter"><div class="gmap_canvas"><iframe width="100%" height="300" id="gmap_canvas" src="https://maps.google.com/maps?q=<?php echo $postcode;?>&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe></div><style>.mapouter{display: inline-block;position:relative;text-align:right;height:300px;width:100%;float:left}.gmap_canvas {display: inline-block;overflow:hidden;background:none!important;height:300px;width:100%;}</style></div>
-</div>
+    </div>
+    </div>
 
-
-
-        </div>
 
 
 
---
+++ review.php
@@ -0,0 +1,33 @@
+
+<!DOCTYPE html>
+<?php
+include "menu.php";
+include "utils/core_functions.php";
+?>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Reviews</title>
+  </head>
+  <body>
+
+ <?php
+
+ if (isset($_POST['submit'])) {
+     $sql = "INSERT INTO comments VALUES('','','')";
+     $review_update = $mysqli->query($sql);
+     header("Refresh:0");
+ }
+ ?>
+
+
+  <form action="" method="post">
+      <input type="text" name="name" value="Uw naam"><br>
+      <textarea name="comment" cols="50" rows="5">Laat een review achter</textarea><br>
+      <input type="submit" value="Laat review achter!">
+  </form>
+
+
+
+  </body>
+</html>
