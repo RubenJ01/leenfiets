fec1ea22ed3b2fbfc4ab731a12c3b96b0e6ab40c
TimBunk <timbunk12@kpnmail.nl>
2019-11-03 19:19:36 +0100

    Als een fiets nog niet in gebruik is kan die altijd nog geannuleerd worden

--- leen_verzoeken.php
+++ leen_verzoeken.php
@@ -1,6 +1,7 @@
 <?php
 include "menu.php";
 require_once "utils/qrcode.php";
+require_once "utils/process_leen_verzoek.php";
 
 if (!isset($_SESSION)) {
     session_start();
@@ -35,6 +36,10 @@ if (isset($_GET['leen_verzoek']) && isset($_GET['token'])) {
     $stmt->close();
   }
 }
+
+// Het kan zijn dat een fiets verlopen is dus daarom roepen we de functie Updatebicycles aan die checkt of er ook iets verlopen is
+UpdateBicycles($_SESSION['id']);
+
 ?>
 
 <!DOCTYPE html>
@@ -45,7 +50,12 @@ if (isset($_GET['leen_verzoek']) && isset($_GET['token'])) {
   <body>
     <div class="wrapper">
       <div id="leen_verzoeken">
-        <h1 class="titel">Leenverzoeken</h1>
+
+
+        <!-- GEKREGEN LEENVERZOEKEN -->
+
+
+        <h1 class="titel">Gekregen leenverzoeken</h1>
         <table>
           <tr>
             <th> Afbeelding </th>
@@ -83,8 +93,8 @@ if (isset($_GET['leen_verzoek']) && isset($_GET['token'])) {
               else { $foto .= "?t=" .time(); }
               if ($status === "in_afwachting") {
                 $status = "<form method='post' action='utils/process_leen_verzoek.php'>
-                            <input type='submit' name='geaccepteerd' value='accepteer'>
-                            <input type='submit' name='geweigerd' value='weiger'>
+                            <input type='submit' name='geaccepteerd' value='accepteer' class='wordBreakDownButton'>
+                            <input type='submit' name='geannuleerd' value='annuleer' class='wordBreakDownButton'>
                             <input type='number' name='id' value='$leenVerzoekId' style='display: none;'>
                           </form>";
               }
@@ -92,6 +102,10 @@ if (isset($_GET['leen_verzoek']) && isset($_GET['token'])) {
                 if ($leenVerzoekToken === NULL) { $qrTokens["{$leenVerzoekId}"] = $token;$token = GetToken(); }
                 else { $token = $leenVerzoekToken; }
                 $status .= ": <a href='qr.php?leen_verzoek=$leenVerzoekId&token=$token'> Klik op deze link en laat de qr code scannen door de lener </a>";
+                $status .= "<form method='post' action='utils/process_leen_verzoek.php'>
+                              <input type='submit' name='geannuleerd' value='of klik hier om te annuleren' class='wordBreakDownButton'>
+                              <input type='number' name='id' value='$leenVerzoekId' style='display: none;'>
+                            </form>";
               }
               echo "
               <tr>
@@ -113,7 +127,130 @@ if (isset($_GET['leen_verzoek']) && isset($_GET['token'])) {
           }
           ?>
         </table>
-        <h1 class="titel">Geleent</h1>
+
+
+        <!-- GESCHIEDENIS UITGELEENDE FIETSEN -->
+
+
+        <h3 class="titel">Geschiedenis uitgeleende fietsen</h3>
+        <table>
+          <tr>
+            <th> Afbeelding </th>
+            <th> Fiets titel </th>
+            <th> status </th>
+            <th> Bericht </th>
+            <th> Prijs per dag </th>
+            <th> Ophaalmoment </th>
+            <th> Terugbrengmoment </th>
+            <th> Lener </th>
+          </tr>
+        <?php
+        $query = "SELECT l.id, l.token, g.id, f.id, f.foto, m.merk_naam, f.model, l.status_, l.bericht, l.prijs, l.ophaal_moment, l.terugbreng_moment, g.naam
+                  FROM leen_verzoek l
+                  LEFT JOIN fietsen f ON f.id = l.fiets_id
+                  LEFT JOIN merk_fiets m ON f.id_merk_fiets = m.id
+                  LEFT JOIN gebruiker g ON g.id = l.lener_id
+                  WHERE f.gebruiker_id = ? AND (l.status_ = 'geannuleerd' OR l.status_ = 'verlopen' OR l.status_ = 'teruggebracht')";
+        $stmt = $GLOBALS['mysqli']->prepare($query);
+        if (!$stmt) {
+          trigger_error($GLOBALS['mysqli']->error, E_USER_ERROR);
+        }
+        else {
+          $stmt->bind_param('i', $_SESSION['id']);
+          if (!$stmt->execute()) {
+            echo "ERROR in query";
+            return;
+          }
+          // Echo all de opgehaalde rijen
+          $stmt->bind_result($leenVerzoekId, $leenVerzoekToken, $gebruikerId, $fietsId, $foto, $merk_naam, $model, $status, $bericht, $prijs, $ophaal_moment, $terugbreng_moment, $lener);
+          $token = "";
+          $qrTokens = array();
+          while ($stmt->fetch()) {
+            if (empty($foto)) { $foto = 'fiets_afbeeldingen/default.png'; }
+            else { $foto .= "?t=" .time(); }
+            echo "
+            <tr>
+              <td> <img src='$foto' style='width:100%; height: auto;'> </td>
+              <td> <a href='fiets.php?fiets_id=$fietsId'> $merk_naam $model </a> </td>
+              <td> $status </td>
+              <td> $bericht </td>
+              <td> € $prijs </td>
+              <td> $ophaal_moment </td>
+              <td> $terugbreng_moment </td>
+              <td> <a href='profiel.php?gebruikers_id=$gebruikerId'> $lener </a></td>
+            </tr>";
+          }
+          $stmt->close();
+        }
+        ?>
+        </table>
+
+
+        <!-- FIETSEN DIE U WILT LENEN OF LEENT -->
+
+
+        <h1 class="titel">Fietsen die u wilt lenen of leent</h1>
+        <table>
+          <tr>
+            <th> Afbeelding </th>
+            <th> Fiets titel </th>
+            <th> status </th>
+            <th> Bericht </th>
+            <th> Prijs per dag </th>
+            <th> Ophaalmoment </th>
+            <th> Terugbrengmoment </th>
+            <th> Eigenaar </th>
+          </tr>
+          <?php
+          $query = "SELECT l.id, f.foto, m.merk_naam, f.model, l.status_, l.bericht, l.prijs, l.ophaal_moment, l.terugbreng_moment, g.naam, g.id
+                    FROM leen_verzoek l
+                    LEFT JOIN fietsen f ON f.id = l.fiets_id
+                    LEFT JOIN merk_fiets m ON f.id_merk_fiets = m.id
+                    LEFT JOIN gebruiker g ON f.gebruiker_id = g.id
+                    WHERE l.lener_id = ? AND (l.status_ = 'in_afwachting' OR l.status_ = 'gereserveerd' OR l.status_ = 'in_gebruik')";
+          $stmt = $GLOBALS['mysqli']->prepare($query);
+          if (!$stmt) {
+            trigger_error($GLOBALS['mysqli']->error, E_USER_ERROR);
+          }
+          else {
+            $stmt->bind_param('i', $_SESSION['id']);
+            if (!$stmt->execute()) {
+              echo "ERROR in query";
+              return;
+            }
+            // Echo all de opgehaalde rijen
+            $stmt->bind_result($leenVerzoekId, $foto, $merk_naam, $model, $status, $bericht, $prijs, $ophaal_moment, $terugbreng_moment, $eigenaar, $eigenaarId);
+            while ($stmt->fetch()) {
+              if (empty($foto)) { $foto = 'fiets_afbeeldingen/default.png'; }
+              else { $foto .= "?t=" .time(); }
+              if ($status === "in_afwachting" || $status === "gereserveerd") {
+                $status .= "<form method='post' action='utils/process_leen_verzoek.php'>
+                            <input type='submit' name='geannuleerd' value='of klik hier om te annuleren' class='wordBreakDownButton'>
+                            <input type='number' name='id' value='$leenVerzoekId' style='display: none;'>
+                          </form>";
+              }
+              echo "
+              <tr>
+                <td> <img src='$foto' style='width:100%; height: auto;'> </td>
+                <td> <a href='fiets.php?fiets_id=$fietsId'> $merk_naam $model </a> </td>
+                <td> $status </td>
+                <td> $bericht </td>
+                <td> € $prijs </td>
+                <td> $ophaal_moment </td>
+                <td> $terugbreng_moment </td>
+                <td> <a href='profiel.php?gebruikers_id=$eigenaarId'> $eigenaar </td>
+              </tr>";
+            }
+            $stmt->close();
+          }
+          ?>
+        </table>
+
+
+        <!-- GESCHIEDENIS GELEENDE FIETSEN -->
+
+
+        <h3 class="titel">Geschiedenis geleende fietsen</h3>
         <table>
           <tr>
             <th> Afbeelding </th>
@@ -131,7 +268,7 @@ if (isset($_GET['leen_verzoek']) && isset($_GET['token'])) {
                     LEFT JOIN fietsen f ON f.id = l.fiets_id
                     LEFT JOIN merk_fiets m ON f.id_merk_fiets = m.id
                     LEFT JOIN gebruiker g ON f.gebruiker_id = g.id
-                    WHERE l.lener_id = ? AND (l.status_ = 'in_afwachting' OR l.status_ = 'gereserveerd' OR l.status_ = 'in_gebruik')";
+                    WHERE l.lener_id = ? AND (l.status_ = 'geannuleerd' OR l.status_ = 'verlopen' OR l.status_ = 'teruggebracht')";
           $stmt = $GLOBALS['mysqli']->prepare($query);
           if (!$stmt) {
             trigger_error($GLOBALS['mysqli']->error, E_USER_ERROR);
@@ -162,19 +299,6 @@ if (isset($_GET['leen_verzoek']) && isset($_GET['token'])) {
             $stmt->close();
           }
           ?>
-        </table>
-        <h3 class="titel">Geschiedenis</h3>
-        <table>
-          <tr>
-            <th> Afbeelding </th>
-            <th> Fiets titel </th>
-            <th> status </th>
-            <th> Bericht </th>
-            <th> Prijs per dag </th>
-            <th> Ophaalmoment </th>
-            <th> Terugbrengmoment </th>
-            <th> Eigenaar </th>
-          </tr>
         </table>
       </div>
     </div>
--- utils/process_leen_verzoek.php
+++ utils/process_leen_verzoek.php
@@ -15,6 +15,30 @@
  }
  require_once $db_conn_file;
 
+ function UpdateBicycles($eigenaarId) {
+   $query = "UPDATE leen_verzoek l
+             SET l.status_ = 'verlopen'
+             WHERE (l.fiets_id IN (
+               SELECT f.id
+               FROM fietsen f
+               WHERE f.gebruiker_id = ?
+             ) OR l.lener_id = ?) AND l.status_ = 'in_afwachting' AND l.ophaal_moment < NOW()
+             ";
+   $stmt = $GLOBALS['mysqli']->prepare($query);
+   if (!$stmt) {
+     trigger_error($GLOBALS['mysqli']->error, E_USER_ERROR);
+   }
+   else {
+     $stmt->bind_param('ii', $eigenaarId, $eigenaarId);
+     if (!$stmt->execute()) {
+       trigger_error($stmt->error, E_USER_ERROR);
+     }
+     $stmt->close();
+     return true;
+   }
+   return false;
+ }
+
  function AcceptRequest($eigenaarId, $vezoekId) {
    $query = "UPDATE leen_verzoek l
              SET l.status_ = 'gereserveerd'
@@ -39,21 +63,21 @@
    return false;
  }
 
- function DenyRequest($eigenaarId, $vezoekId) {
+ function DenyRequest($gebruikerId, $vezoekId) {
    $query = "UPDATE leen_verzoek l
-             SET l.status_ = 'geweigerd'
-             WHERE l.id = ? AND l.status_ = 'in_afwachting' AND ? IN (
+             SET l.status_ = 'geannuleerd'
+             WHERE l.id = ? AND (l.status_ = 'in_afwachting' OR l.status_ = 'gereserveerd') AND (? IN (
                SELECT f.gebruiker_id
                FROM fietsen f
                WHERE l.id = ? AND l.fiets_id = f.id AND f.gebruiker_id = ?
-             )";
+             ) OR l.lener_id = ?)";
 
    $stmt = $GLOBALS['mysqli']->prepare($query);
    if (!$stmt) {
      trigger_error($GLOBALS['mysqli']->error, E_USER_ERROR);
    }
    else {
-     $stmt->bind_param('iiii', $vezoekId, $eigenaarId, $vezoekId, $eigenaarId);
+     $stmt->bind_param('iiiii', $vezoekId, $gebruikerId, $vezoekId, $gebruikerId, $gebruikerId);
      if (!$stmt->execute()) {
        trigger_error($stmt->error, E_USER_ERROR);
      }
@@ -77,8 +101,8 @@
    header("Location: ../leen_verzoeken.php");
   }
 
-  // Check of er op geweigerd is geklikt
-  if(isset($_POST['geweigerd'])) {
+  // Check of er op geannuleerd is geklikt
+  if(isset($_POST['geannuleerd'])) {
    // Start de sessie mocht dat nog niet gedaan zijn
    if (isset($_SESSION) === false) {
        session_start();
